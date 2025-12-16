a9af0bad19fd9aa131a35716bc4b7f87fd3d093a
Danyal494 <m.danyalkhan916@gmail.com>
2025-12-16 11:11:50 +0500

    updated loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
-import { helix } from 'ldrs';
+import React from "react";
+import { helix } from "ldrs";
+import { useProgress } from "@react-three/drei";
 
 helix.register();
 
 const Loader = () => {
+  const { progress } = useProgress();
+
   return (
-    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <l-helix size="80" speed="2.5" color="white"></l-helix>
+      <p style={{ color: "white", marginTop: "22px" }}>
+        {progress.toFixed(0)}%
+      </p>
     </div>
   );
 };
